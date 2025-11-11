3bd3f4f75d901e4539b3740eb24ad9e85d60416f
zryuik <yuristronda123@hotmail.com>
2025-11-11 10:59:38 -0300

    aprendi sobre modulos e pacotes nessa aula

---
+++ aula22.py
@@ -0,0 +1,9 @@
+from modulos.uteis import numeros
+
+
+
+num = int(input("Digite um valor: "))
+fat = numeros.fatorial(num)
+print(f"O fatorial de {num} é {fat}")
+print(f"O dobro de {num} é {numeros.dobro(num)}")
+print(f"O dobro de {num} é {numeros.triplo(num)}")
